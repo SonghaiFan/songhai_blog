93d247c3b51512cbedc1d94b169cd5901c65c756
SonghaiFan <songhai.fan2022@gmail.com>
2025-01-25 00:49:19 +1100

    blog update

--- scripts/blogIndex.js
+++ scripts/blogIndex.js
@@ -13,6 +13,31 @@ const enBlogIndex = [
 
 const zhBlogIndex = [
   {
+    "text": "AI",
+    "items": [
+      {
+        "text": "值得关注的AI应用开源项目",
+        "link": "/zh/blog/posts/AI应用开源项目-2025-01-24-22-38-58",
+        "date": "2025-01-24T22:38:58.000Z"
+      }
+    ]
+  },
+  {
+    "text": "PhD",
+    "items": [
+      {
+        "text": "TTNG研究的三个阶段",
+        "link": "/zh/blog/posts/ttng三阶段-2025-01-23-10-14-51",
+        "date": "2025-01-23T23:14:51.455Z"
+      },
+      {
+        "text": "博士开题思路总结",
+        "link": "/zh/blog/posts/博士开题思路总结-2025-01-23-17-00-35",
+        "date": "2025-01-23T06:00:35.232Z"
+      }
+    ]
+  },
+  {
     "text": "VitePress",
     "items": [
       {
@@ -23,12 +48,12 @@ const zhBlogIndex = [
     ]
   },
   {
-    "text": "Uncategorized",
+    "text": "设计系统",
     "items": [
       {
-        "text": "博士开题思路总结",
-        "link": "/zh/blog/posts/博士开题思路总结-2025-01-23-17-00-35",
-        "date": "2025-01-23T06:00:35.232Z"
+        "text": "主流设计系统与组件库对比分析",
+        "link": "/zh/blog/posts/主流设计系统与组件库对比分析-2025-01-24-00-30-58",
+        "date": "2025-01-24T13:30:58.826Z"
       }
     ]
   }
